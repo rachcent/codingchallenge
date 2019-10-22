3b5e8d47d3b8502208be21606283a3866999042c
rachcent <35813484+rachcent@users.noreply.github.com>
2019-10-22 16:15:20 -0700

    update

--- sketch.js
+++ sketch.js
@@ -1,7 +1,20 @@
+let button;
+let img;
+
+function preload(){
+  //load images
+  img =loadImage("bg_1.jpg");
+}
+
 function setup() {
   createCanvas(400, 400);
   createButton("click to change background");
-  
+  button.position(19,19);
+  button.mousePressed(ChangeBG);
+}
+
+function changeBG(){
+  let img =loadImage("bg_1.jpg")
 }
 
 function draw() {
